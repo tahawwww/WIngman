bc5a1747bd14269fc64b49c1c61be577847029b8
tahawwww <140329975+tahawwww@users.noreply.github.com>
2024-12-21 00:53:50 +0500

    Update Game.h

--- Game/Game.h
+++ Game/Game.h
@@ -1,30 +1,71 @@
+#ifndef GAME_H
+#define GAME_H
 #include <iostream>
-#include "Game.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <vector>
+#include "Enemy.h"
+
 
 using namespace sf;
-using namespace std;
 
-int main()
-{
-    RenderWindow window(VideoMode(1920,1080),"Wingman By Taha Manan",Style::Default);
+class Player;
+class Game
+{   private:
+        RenderWindow *window;
 
+        // Wallpaper
+        Texture wallpaperTexture;
+        Sprite wallpaperSprite;
 
-    Game game(&window);
-    // Game Loop
-    while(window.isOpen())
-    {
-        Event event;
-        while(window.pollEvent(event))
+        // Text
+        Font font;
+        std::vector<Text> followPlayerTexts;
+        std::vector<Text> staticPlayerTexts;
+        Text enemyText;
+        Text gameOverText;
+
+        //Players
+        std::vector<Player> players;
+        int playersAlive;
+
+        // Enemies
+        std::vector<Enemy> enemies;
+        std::vector<Enemy> enemiesSaved;
+        int enemySpawnTimerMax;
+        int enemySpawnTimer;
+
+        //Textures
+        std::vector<Texture> textures;
+
+    public:
+        Game(RenderWindow *window);
+        virtual ~Game();
+
+        // Accessors
+        inline RenderWindow &getWindow()
         {
-            if(event.type == Event::Closed)
-            {window.close();}
-            if(event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
-            {window.close();}
+            return *this->window;
         }
 
-        game.Update();
-        game.Draw();
+        // Setters
 
-    }
-    return 0;
-}
+
+        // Functions
+        void InitUI();
+        void DrawUI();
+        void UpdateUI();
+
+        void Update();
+        void Draw();
+
+    protected:
+
+
+};
+
+#endif // GAME_H
+
